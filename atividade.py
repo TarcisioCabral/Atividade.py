7b2b2e4ed477423f6199bff58d5493b2228ac2e5
Tarcisio Cabral <72229357+TarcisioCabral@users.noreply.github.com>
2022-03-10 22:54:08 -0300

    Create atividade.py

---
+++ atividade.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from apriori_python import apriori
+
+heart = pd.read_csv('heart.csv')
+
+agrupamento = heart.groupby('Cholesterol')['ChestPainType'].apply(list)
+
+print(heart.head())
+
+freqItemSet,rules=apriori(agrupamento, minSup=0.000909, minConf=0.5)
+print(len(agrupamento))
+print(rules)
